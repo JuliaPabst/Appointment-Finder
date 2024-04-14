f9d089e1f0411a8ff4950127cb9e2c0949d6f668
Felix Schober <if23b087@technikum-wien.at>
2024-04-14 05:56:10 +0200

    added TimeSlot Class

--- Server/models/appointment.php
+++ Server/models/appointment.php
@@ -15,3 +15,19 @@ class Appointment {
 
       }
 }
+
+class TimeSlot {
+  public $id;
+  public $date;
+  public $begin_time;
+  public $end_time;
+  public $fk_appointment_id;
+  function __construct($id, $date, $begin_time, $end_time, $fk_appointment_id) {
+      $this->id = $id;
+      $this->date = $date;
+      $this->begin_time=$begin_time;
+      $this->end_time=$end_time;
+      $this->fk_appointment_id=$fk_appointment_id;
+
+    }
+}
