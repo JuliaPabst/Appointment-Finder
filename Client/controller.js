2fd10149a64e507531435c000cdada2ed9823472
Julia Pabst <ju.pabst@gmail.com>
2024-04-15 11:48:07 +0200

    work on form

--- Client/controller.js
+++ Client/controller.js
@@ -8,8 +8,9 @@ $(document).ready(function () {
     });
 
     $("#newAppointment").click(function (e) {
-        createNewAppointment();
+        showNewAppointmentForm();
     });
+
 
     showOverview();
 });
@@ -45,10 +46,10 @@ function showOverview(){
                 var appointment = `
 
                 <div class="row appointment">
-                <h4>${appointment[0].title}</h4>
+                <h4 class="singleAppointment" onClick="showSingleAppointment(event)">${appointment[0].title}</h4>
                 <ul>
                 <li>Title: ${appointment[0].title}</li>
-                <li>Date: ${appointment[0].date}</li>
+                <li>Created at: ${appointment[0].date}</li>
                 <li>Location: ${appointment[0].location}</li>
                 <li>Title ${appointment[0].expiration_date}</li>
                 </ul>
@@ -64,23 +65,58 @@ function showOverview(){
 
 
 // post new appointment to database 
-function createNewAppointment(){
+function showNewAppointmentForm(){
     console.log("create");
-    let createContainer = $("<div class='createContainer'></div>");
-    let titleLabel = $("<label for='title' id='create-titleLabel'>Title</label>");
-    let titleInput = $("<input name='title' id='create-titleInput'>");
-    let locationLabel = $("<label for='location' id='create-locationLabel'>Location</label>");
-    let locationInput = $("<input name='location' id='create-locationInput'>");
-    let dateLabel = $("<input type ='date' name='date' id='create-dateLabel'>Date</label>");
-    let dateInput = $("<input name='date' id='create-dateInput'>");
-    let expirationDateLabel = $("<input type ='date' name='expirationDate' id='create-expirationDateLabel'>Expiration Date</label>");
-    let expirationDateInput = $("<input name='expirationDate' id='create-expirationDateInput'>");
 
-    createContainer.append(titleLabel, titleInput, locationLabel, locationInput, dateLabel, dateInput, expirationDateLabel, expirationDateInput);
+    let createContainer = $("<form id='createForm'></form>");
+    let title = $("<div id='create-title'><label for='title' id='create-titleLabel'>Title</label><input name='title' id='create-titleInput'></div>")
+    let location = $("<div id='create-location'><label for='location' id='create-locationLabel'>Location</label><input name='location' id='create-locationInput'></div>")
+    let date = $("<div id='create-date'><label for='date' id='create-dateLabel'>Date</label><input type='date' name='date' id='create-dateInput'></div>")
+    let expiration_date = $("<div id='create-expirationDate'><label for='expiration_date' id='create-expirationDateLabel'>Expiration Date</label><input type='date' name='expiration_date' id='create-expirationDateInput'></div>");
+    let submitButton = $("<button type='submit'>Submit</button>");
+    createContainer.append(title, location, date, expiration_date, submitButton);
     $('#createAppointment').append(createContainer);
+
+    $("#createForm").on("submit", function (e) {
+        e.preventDefault();
+        submitNewAppointment(e)
+    });
 }
 
-// load all options for one specific appointment 
-function showSingleAppointment(){
+
+function submitNewAppointment(e){
+    e.preventDefault();
+
+    let formDataArray = $(e.target).serializeArray();
+
+    let formDataObject = {};
+    $.each(formDataArray, function(index, item) {
+      formDataObject[item.name] = item.value;
+    });
+
+    $('#createForm').remove();
+    console.log(formDataObject);
+
+    // server endpoint to be done
+    /*
+    $.ajax({
+      type: 'POST',
+      url: 'your-server-endpoint.php', 
+      data: formDataObject,
+      success: function(response) {
+        console.log(response);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error('AJAX error: ', textStatus, ' : ', errorThrown);
+      }
+    }); 
+  });
+  */
+}
+ 
+function showSingleAppointment(event){
+$("#fullPage").hide();
+console.log($(event.target).text());
+
 }
 
