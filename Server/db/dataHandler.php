a868a59bd7a3610f26c156bc8d69c16cd81051b5
Felix Schober <if23b087@technikum-wien.at>
2024-04-25 18:12:02 +0200

    demoData is now defined as private variables, instead of inside the getter Function

--- Server/db/dataHandler.php
+++ Server/db/dataHandler.php
@@ -2,15 +2,32 @@
 include("./models/appointment.php");
 class DataHandler
 {
+    private $demoAppointments;
+    private $demoTimeSlots;
+    public function __construct()
+    {
+        $this->demoAppointments = [
+            [new Appointment(1, "StudySession", "FH", "03.04.2024", "30.04.2024")],
+            [new Appointment(2, "Workout", "GymBros", "29.04.2025", "30.04.2024")],
+        ];
+
+        $this->demoTimeSlots = [
+            [new TimeSlot(1, '2024-04-14', '09:00', '10:00', 1)],
+            [new TimeSlot(2, '2024-04-14', '10:00', '11:00', 1)],
+            [new TimeSlot(3, '2024-04-15', '11:00', '12:00', 2)],
+            [new TimeSlot(4, '2024-04-15', '13:00', '14:00', 2)],
+            [new TimeSlot(5, '2024-04-16', '14:00', '15:00', 2)]
+        ];
+    }
     public function queryAppointments()
     {
-        $res =  $this->getDemoData();
+        $res =  $this->demoAppointments;
         return $res;
     }
 
     public function queryTimeslots()
     {
-        $res =  $this->getTimeSlotsDemoData();
+        $res =  $this->demoTimeSlots;
         return $res;
     }
 
@@ -55,25 +72,5 @@ class DataHandler
         return $result;
     }
 
-    private static function getDemoData()
-    {
-        $demodata = [
-            [new Appointment(1, "StudySession", "FH", "03.04.2024", "30.04.2024")],
-            [new Appointment(2, "Workout", "GymBros", "29.04.2025", "30.04.2024")],
-        ];
-        return $demodata;
-    }
-
-    private static function getTimeSlotsDemoData()
-    {
-        $demodata = [
-            [new TimeSlot(1, '2024-04-14', '09:00', '10:00', 1)],
-            [new TimeSlot(2, '2024-04-14', '10:00', '11:00', 1)],
-            [new TimeSlot(3, '2024-04-15', '11:00', '12:00', 2)],
-            [new TimeSlot(4, '2024-04-15', '13:00', '14:00', 2)],
-            [new TimeSlot(5, '2024-04-16', '14:00', '15:00', 2)]
-			
-        ];
-        return $demodata;
-    }
+    
 }
